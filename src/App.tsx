3ff82ea86f49a58ef907e8cdbe90089cdc3c31aa
liujuncai <1769031928@qq.com>
2022-05-10 14:21:51 +0800

    feat: feat home welcome ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
+import styled from 'styled-components'
 
-function Some() {
-  return <div>hello</div>
-}
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: skyblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`
 export default function App() {
-  return (
-    <div>
-      <Some />
-    </div>
-  )
+  return <Wrapper>Hi guy,welcome to my site!</Wrapper>
 }
