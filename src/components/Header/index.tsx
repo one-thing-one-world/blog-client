238566620cfd0bb8d56b38c1d8fc447aa6adb971
liujuncai <1769031928@qq.com>
2022-05-18 09:58:10 +0800

    fix: fix ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
-// import logo from '../../assets/logo.jpg'
+import styled, { keyframes } from 'styled-components'
+import logo from '../../assets/logo.jpg'
 import NavComponent from './NavComponent'
 import NavUserInfo from './NavUserInfo'
 
+const imgRotate = keyframes`
+/* 0% {
+      transform: scale(90deg);
+    }
+    25% {
+      transform: scale(180deg);
+    }
+    75% {
+      transform: scale(270deg);
+    }
+    100% {
+      transform: scale(360deg);
+    } */
+    from {
+      transform: rotate(0deg);
+     }
+
+    to {
+      transform: rotate(360deg);
+    }
+
+`
 const Wrapper = styled.div`
   width: 100%;
   height: 60px;
@@ -11,16 +33,16 @@ const Wrapper = styled.div`
   justify-content: space-around;
   align-items: center;
 `
-// const LogoWrapper = styled.img`
-//   width: 60px;
-//   height: 60px;
-//   border-radius: 50% 50%;
-// `
-
+const LogoWrapper = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50% 50%;
+  animation: ${imgRotate} 3s linear infinite;
+`
 export default function Header() {
   return (
     <Wrapper>
-      {/* <LogoWrapper src={logo} /> */}
+      <LogoWrapper src={logo} />
       <NavComponent />
       <NavUserInfo />
     </Wrapper>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,17 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-// import { Button, Typography } from '@mui/material'
-// import CenterWrapper from '../../style/common'
 // import { useNavigate } from 'react-router-dom'
 
 import HomeCom from './HomeCom'
-// import MetaphysicsCom from './MetaphysicsCom/index'
-// import ScienceCom from './ScienceCom'
-// import TechCom from './TechCom'
-// import TrivialCom from './TrivialCom'
-// import WriteArticalCom from './WriteArticalCom'
+import MetaphysicsCom from './MetaphysicsCom/index'
+import ScienceCom from './ScienceCom'
+import TechCom from './TechCom'
+import TrivialCom from './TrivialCom'
+import WriteArticalCom from './WriteArticalCom'
 
 const Wrapper = styled.div`
   font-size: 20px;
 `
-
-// const GoldenWrapper = styled.div`
-//   background: #fffbf0;
-//   width: 100%;
-//   height: 700px;
-// `
-// // const textWrapper = styled.div``
-// const WoodWrapper = styled(GoldenWrapper)`
-//   background: #e8f5e9;
-// `
-
-// const WaterWrapper = styled(GoldenWrapper)`
-//   background: #e3f2fd;
-// `
-// const FlameWrapper = styled(GoldenWrapper)`
-//   background: #ffebee;
-// `
-// const SoilWrapper = styled(GoldenWrapper)`
-//   background: #fffde7;
-// `
 export default function Home() {
   // const navigate = useNavigate()
   useEffect(() => {
@@ -42,11 +20,11 @@ export default function Home() {
   return (
     <Wrapper>
       <HomeCom />
-      {/* <MetaphysicsCom />
+      <MetaphysicsCom />
       <ScienceCom />
       <TechCom />
       <TrivialCom />
-      <WriteArticalCom /> */}
+      <WriteArticalCom />
     </Wrapper>
   )
 }
