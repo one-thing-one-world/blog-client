17694e4f8d2994fac81f27058f228953e3caca92
liujuncai <1769031928@qq.com>
2022-05-20 17:54:26 +0800

    feat: fix ui

--- src/pages/AuthorArtical/index.tsx
+++ src/pages/AuthorArtical/index.tsx
@@ -5,8 +5,12 @@ const Wrapper = styled.div`
   background: #fffbf0;
   width: 100%;
   height: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
 `
 
 export default function AuthorArtical() {
-  return <Wrapper>this is AuthorArtical</Wrapper>
+  return <Wrapper>写文章</Wrapper>
 }
--- src/pages/Trivial/index.tsx
+++ src/pages/Trivial/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Typography } from '@mui/material'
+import CenterWrapper from '../../style/common'
 
-const Wrapper = styled.div`
-  background: #fffbf0;
+const GoldWrapper = styled.div`
+  background: #ffebee;
   width: 100%;
   height: 700px;
 `
 export default function Trivial() {
-  return <Wrapper>Trivial</Wrapper>
+  return (
+    <GoldWrapper>
+      <CenterWrapper>
+        <Typography variant="h4" gutterBottom component="div">
+          生活酸甜苦辣，生活感悟。
+        </Typography>
+      </CenterWrapper>
+    </GoldWrapper>
+  )
 }
