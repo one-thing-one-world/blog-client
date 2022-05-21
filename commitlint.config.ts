12eafe57914423f49e620071179aeb4ce5b8bf8f
liujuncai <1769031928@qq.com>
2022-05-21 17:24:40 +0800

    [Update::commitlint] update commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,22 +1,3 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-      ],
-    ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-  },
+module.exports = {
+  extends: 'wizardoc',
 }
