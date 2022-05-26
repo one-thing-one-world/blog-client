0b9ae2693217fd3776746f4289af4ccff0bf0160
liujuncai <1769031928@qq.com>
2022-05-26 10:51:28 +0800

    [Feat::backtop] media query backtop fixed postion

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,15 +8,20 @@ const OutletWrapper = styled.div`
   max-width: 1020px;
   margin: 0 auto;
 `
+const LayoutWrapper = styled.div`
+  background: #f3f7fd;
+  width: 100%;
+  height: 100%;
+`
 export default function Layout() {
   return (
-    <>
+    <LayoutWrapper>
       <BackTop>
         <Header />
       </BackTop>
       <OutletWrapper>
         <Outlet />
       </OutletWrapper>
-    </>
+    </LayoutWrapper>
   )
 }
