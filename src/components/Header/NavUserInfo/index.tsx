e79a9c714c04186855b69c48e46f3de7014dad47
liujuncai <1769031928@qq.com>
2022-06-13 09:56:33 +0800

    [Feat::github] add  link of github on the header

--- src/components/Header/NavUserInfo/index.tsx
+++ src/components/Header/NavUserInfo/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import { Typography, Box } from '@mui/material'
+
+const GitHubWrapper = styled.img`
+  width: 32px;
+  height: 32px;
+  margin-right: 10px;
+  cursor: pointer;
+`
 
 export default function NavUserInfo() {
   return (
-    <Box sx={{ display: 'flex', height: '60px' }}>
+    <Box sx={{ display: 'flex', alignItems: 'center', height: '60px' }}>
+      <GitHubWrapper
+        onClick={() => {
+          window.location.href = 'https://github.com/one-thing-one-world'
+        }}
+        src="https://www.zzhack.fun/images/$light_github.svg"
+      />
       <Typography
         variant="h6"
         component="div"
