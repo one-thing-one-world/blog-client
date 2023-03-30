b2634ad7c0032b365ff057496f704188a75216a5
liujuncai <1769031928@qq.com>
2023-03-30 15:34:16 +0800

    [Fix] 增加首页定时清除登录状态

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,16 +6,32 @@ import HomeBanner from './HomeBanner'
 import HomeIntro from './HomeIntro'
 import HomeSponsor from './HomeSponsor'
 import HomeEmail from './HomeEmail'
+import { useAppDispatch } from '../../hook/reduxHook'
+import { setterUserInfoStoreState } from '../../store/userInfo'
 
 const Wrapper = styled.div`
   /* font-size: 2000rw !important; */
 `
 export default function Home() {
   // const navigate = useNavigate()
+  const dispatch = useAppDispatch()
   useEffect(() => {
     console.log(window.location, 'home')
   }, [])
-  useEffect(() => {}, [])
+  useEffect(
+    () => () => {
+      // 一天后退出登录
+      setTimeout(() => {
+        dispatch(
+          setterUserInfoStoreState({
+            type: 'isLogin',
+            value: 0,
+          })
+        )
+      }, 1000 * 60 * 60 * 24)
+    },
+    []
+  )
   return (
     <Wrapper>
       <HomeBanner />
