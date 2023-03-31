069666aaf6ad6c8e2c2dce27d4e735573684a856
liujuncai <1769031928@qq.com>
2023-03-31 17:53:48 +0800

    [Feat] 配置打包文件夹路径

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,78 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-undef */
+const { whenProd } = require('@craco/craco')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const path = require('path')
+
+const resolve = dir => path.resolve(__dirname, dir)
+console.log('hello')
 module.exports = {
-  // webpack: {
-  //   alias: {
-  //     '@': resolve('src'),
-  //     components: resolve('src/components'),
-  //   },
-  // },
+  webpack: {
+    alias: {
+      '~': resolve('src'),
+      components: resolve('src/components'),
+    },
+
+    configure: (webpackConfig, { env, paths }) => {
+      // 更改webpack，资源文件夹位置变更 static -> web
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        ...{
+          assetModuleFilename: 'surveylist/assets/[name].[hash][ext]',
+          filename: whenProd(
+            () => 'surveylist/js/[name].[contenthash:8].chunk.js',
+            'surveylist/js/[name].chunk.js'
+          ),
+          chunkFilename: whenProd(
+            () => 'surveylist/js/[name].[contenthash:8].chunk.js',
+            'surveylist/js/[name].chunk.js'
+          ),
+        },
+      }
+
+      webpackConfig.plugins = webpackConfig.plugins.map(plugin => {
+        whenProd(() => {
+          if (plugin instanceof MiniCssExtractPlugin) {
+            Object.assign(plugin.options, {
+              filename: 'surveylist/css/[name].[contenthash:8].css',
+              chunkFilename: 'surveylist/css/[name].[contenthash:8].chunk.css',
+            })
+          }
+        })
+        return plugin
+      })
+      // console.log(rule.oneOf,'oneOf')
+      webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+        console.log(rule, 'ruleout')
+        if (rule.oneOf) {
+          rule.oneOf = rule.oneOf.map((oneOfRule, index) => {
+            console.log(oneOfRule, 'oneOfRule1', index)
+
+            // if (oneOfRule.use) {
+            //   oneOfRule.use[1].options.name = 'statics/media/[name].[hash].[ext]'
+            // }
+            return oneOfRule
+          })
+        }
+        return rule
+      })
+      // iterator
+      // foreach
+      // map pure filter reduce
+
+      for (const rule of webpackConfig.module.rules) {
+        if (!rule.oneOf) {
+          continue
+        }
+
+        rule.oneOf[2].use[1].options.name =
+          'surveylist/assets/[name].[hash].[ext]'
+      }
+
+      return webpackConfig
+    },
+  },
 
   style: {
     postcss: {
