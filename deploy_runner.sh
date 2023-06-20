5eef69eea4611ab4a4fa44234c65b9869c343585
liujuncai <1769031928@qq.com>
2023-06-20 15:56:45 +0800

    [Fix] 修改sh判断

--- deploy_runner.sh
+++ deploy_runner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-if [[ -n $(docker ps -q -f "name=^deploy$") ]]; then
+if [[ -z $(docker ps -q -f "name=^deploy$") ]]; then
+    echo "not install"
+    docker build -t deployfront .
+    docker run --name deploy -i -p 80:80 deployfront
+else
     echo "has install"
     docker stop deploy
     docker rf -f deploy
     docker rmi deployfront
-else
-    docker build -t deployfront .
-    docker run --name deploy -i -p 80:80 deployfront
-    echo "not install"
+
 fi
